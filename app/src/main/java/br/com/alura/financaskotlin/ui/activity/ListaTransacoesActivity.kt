1bf39e0178bd4511fcd63db6594635db56c2e9a0
Cesar Augusto Lima Silva <cesaraugusto.ls@hotmail.com>
2018-08-14 16:49:19 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/financaskotlin/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,14 @@
+package br.com.alura.financaskotlin.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.alura.financaskotlin.R
+
+class ListaTransacoesActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+
+}
